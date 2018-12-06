3fd1a469b21e856394b7b2b4044f46e18cf9148c
Akaan Qualrus <akaan.qualrus@gmail.com>
2018-12-06 10:16:57 +0100

    Added a banner to compiled library.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+let banner = `/**
+ * ArkhamOdds v${pkg.version}
+ * ${pkg.description}
+ * Author: ${pkg.author}
+ */`
 
 export default {
   input: 'src/index.ts',
   output: {
     name: 'ArkhamOdds',
     file: 'dist/arkham-odds.js',
-    format: 'iife'
+    format: 'iife',
+    banner: banner
   },
   plugins: [
     typescript()
