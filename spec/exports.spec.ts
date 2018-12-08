59d3e30b5b6bfb68e5d8a82b2657ec508d756ba8
Akaan Qualrus <akaan.qualrus@gmail.com>
2018-12-08 21:34:40 +0100

    Added first test!

---
+++ spec/exports.spec.ts
@@ -0,0 +1,10 @@
+// tslint:disable:no-unused-expression
+import * as ArkhamOdds from 'arkham-odds';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('exports', () => {
+  it('exports the `odds` function', () => {
+    expect(ArkhamOdds.odds).to.exist;
+  });
+});
