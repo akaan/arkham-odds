ef6188947aa91d80c06cf39e9267c2d33ac71268
Akaan Qualrus <akaan.qualrus@gmail.com>
2020-05-25 13:48:01 +0200

    doc(Fraction): add documentation to Fraction
    Closes #12

--- src/Fraction.ts
+++ src/Fraction.ts
@@ -11,16 +11,36 @@ function gcd(a: number, b: number): number {
   return gcd(a, b - a);
 }
 
+/**
+ * A fraction with a numerator and a denominator.
+ */
 export class Fraction {
   private _numerator: number;
   private _denominator: number;
 
+  /**
+   * Creates and immediately reduce the fraction using the greatest common
+   * divider of the numerator and denominator.
+   *
+   * @param {number} numerator
+   *   The numerator of this fraction.
+   * @param {number} denominator
+   *   The denominator of this fraction.
+   */
   constructor(numerator: number, denominator: number) {
     this._numerator = numerator;
     this._denominator = denominator;
     this._reduce();
   }
 
+  /**
+   * Return a new fraction by adding the given fraction to this fraction.
+   *
+   * @param {Fraction} other
+   *   The fraction to add to this one.
+   * @return {Fraction}
+   *   A new fraction obtained by adding the 2 fractions.
+   */
   public add(other: Fraction): Fraction {
     return new Fraction(
       this._numerator * other._denominator +
@@ -29,10 +49,24 @@ export class Fraction {
     );
   }
 
+  /**
+   * Returns the number value of this fraction.
+   *
+   * @return {number}
+   *   The number value of this fraction
+   */
   public valueOf(): number {
     return this._numerator / this._denominator;
   }
 
+  /**
+   * Checks if this fraction is equal to the provided fraction.
+   *
+   * @param {Fraction} other
+   *   The fraction to compare to.
+   * @return {boolean}
+   *   `true` if the 2 fractions are equal, `false` else.
+   */
   public sameAs(other: Fraction): boolean {
     this._reduce();
     other._reduce();
@@ -42,6 +76,10 @@ export class Fraction {
     );
   }
 
+  /**
+   * Reduce the fraction using the greatest common divider of the numerator
+   * and the denominator.
+   */
   private _reduce(): void {
     const div = gcd(this._numerator, this._denominator);
     if (div) {
