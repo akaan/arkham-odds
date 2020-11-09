1419178cb275efa809bc78d5b7c01f17e590c00a
Akaan Qualrus <akaan.qualrus@gmail.com>
2020-11-09 10:50:00 +0100

    fix(Fraction): better implementation of `gcd`
    Improved implementation for `gcd` to avoid stack size errors.

--- src/Fraction.ts
+++ src/Fraction.ts
@@ -1,14 +1,13 @@
 function gcd(a: number, b: number): number {
-  if (a === 0 || b === 0) {
-    return 0;
+  if (b > a) {
+    return gcd(b, a);
   }
-  if (a === b) {
+
+  if (b === 0) {
     return a;
+  } else {
+    return gcd(b, a % b);
   }
-  if (a > b) {
-    return gcd(a - b, b);
-  }
-  return gcd(a, b - a);
 }
 
 /**
